425aa0063a0f0d6a5800471f11804f8516cb86e0
Jongseung Lim <penandlim@gmail.com>
2021-10-07 02:13:24 -0400

    Fix comparison typo

--- contracts/Vesting.sol
+++ contracts/Vesting.sol
@@ -68,7 +68,7 @@ contract Vesting is Initializable, Context {
         uint256 _cliffInSeconds,
         uint256 _durationInSeconds
     ) external initializer {
-        require(_token == address(0), "_token cannot be empty");
+        require(_token != address(0), "_token cannot be empty");
         // dev: beneficiary is set to msg.sender on logic contracts during deployment
         require(beneficiary == address(0), "cannot initialize logic contract");
         require(_beneficiary != address(0), "_beneficiary cannot be empty");
