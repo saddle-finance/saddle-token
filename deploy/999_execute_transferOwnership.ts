a24f6a5348ca903b8e9a763ddd47f0d6387ffb38
Jongseung Lim <penandlim@gmail.com>
2021-11-15 15:34:40 -0500

    Remove ownership transfer until L2 deployments are done manually

--- deploy/999_execute_transferOwnership.ts
+++ deploy/999_execute_transferOwnership.ts
@@ -13,22 +13,22 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const MULTISIG = "0x3F8E527aF4e0c6e763e8f368AC679c44C45626aE"
 
   // Change SDL governance to multisig
-  await execute(
-    "SDL",
-    { from: deployer, log: true },
-    "changeGovernance",
-    MULTISIG,
-  )
+  // await execute(
+  //   "SDL",
+  //   { from: deployer, log: true },
+  //   "changeGovernance",
+  //   MULTISIG,
+  // )
 
   // Change Minichef owner to multisig
-  await execute(
-    "MiniChefV2",
-    { from: deployer, log: true },
-    "transferOwnership",
-    MULTISIG,
-    false,
-    false,
-  )
+  // await execute(
+  //   "MiniChefV2",
+  //   { from: deployer, log: true },
+  //   "transferOwnership",
+  //   MULTISIG,
+  //   false,
+  //   false,
+  // )
 }
 export default func
 func.tags = ["TransferOwnership"]
